f902623a84398f1fefebecb62ae935b8e7a7d81f
Igor Pozdeev <pozdeev.igor@gmail.com>
2019-12-29 22:11:47 +0400

    pause menu

---
+++ Assets/Code/UI/PauseMenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public sealed class PauseMenuManager : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup pauseGroup;
+
+    // TODO: replace with atom event
+    public void OnBackButton()
+    {
+        UnPause();
+    }
+
+    public void OnQuitButton()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0.0f;
+        pauseGroup.alpha = 0f;
+        pauseGroup.gameObject.SetActive(true);
+        pauseGroup.DOFade(1.0f, 0.25f).SetUpdate(true);
+    }
+
+    public void UnPause()
+    {
+        pauseGroup.DOFade(0.0f, 0.25f).SetUpdate(true).OnComplete(() => { gameObject.SetActive(false); Time.timeScale = 1.0f; } );
+    }
+}
